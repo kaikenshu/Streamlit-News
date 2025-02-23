f70d2443b216cc2d615aaaa8ee985784f99a43ce
Kaiken Shu <kaikenshu@berkeley.edu>
2025-02-22 22:26:06 -0800

    v0

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import streamlit as st
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+# uri = st.secrets["uri"]
+uri = "mongodb+srv://nutrition:tracker@storage.vna8d.mongodb.net/?retryWrites=true&w=majority&appName=Storage"
+
+mongo = MongoClient(uri, server_api=ServerApi('1'))
+
+db = mongo.get_database("news")
+collection = db.get_collection("summaries")
+
+st.write(collection.find())
