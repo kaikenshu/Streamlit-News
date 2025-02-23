14126cbbc2374b2dfd7823a599f8d050cf562010
Kaiken Shu <kaikenshu@berkeley.edu>
2025-02-22 22:48:31 -0800

    v1

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import streamlit as st
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-# uri = st.secrets["uri"]
-uri = "mongodb+srv://nutrition:tracker@storage.vna8d.mongodb.net/?retryWrites=true&w=majority&appName=Storage"
+uri = st.secrets["uri"]
 
 mongo = MongoClient(uri, server_api=ServerApi('1'))
 
 db = mongo.get_database("news")
 collection = db.get_collection("summaries")
 
-st.write(collection.find())
+doc_limit = 3
+
+docs = collection.find().sort("_id", -1).limit(doc_limit)
+
+def print_colored_dict(data):
+    for key, value in data.items():
+        st.markdown(f"<span style='color:white'>{key}:</span> <span style='color:white'>{value}</span>", unsafe_allow_html=True)
+
+
+# st.markdown(f'<h1 style="color:white;font-size:24px;">{"ColorMeBlue text”"}</h1>', unsafe_allow_html=True)
+
+for doc in docs:
+    # st.write(doc)
+    print_colored_dict(doc)
